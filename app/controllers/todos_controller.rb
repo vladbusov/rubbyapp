c825c0cc1aed6fd69d0bdbae310174d44850a34a
vladbusov <to2002to2002@gmail.com>
2019-01-23 13:31:09 +0300

    problems

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,13 +1,46 @@
 class TodosController < ApplicationController
 
+  def index
+    @todo = Todo.all
+  end
+
+  # GET /projects/1
+  # GET /projects/1.json
+  def show
+  end
+
+  # GET /projects/new
+  def new
+    @todo = Todo.new
+  end
+
+  # GET /projects/1/edit
+  def edit
+  end
+
   def create
     @todo = Todo.new(todo_params)
     @todo.save
     redirect_to projects_url
   end
 
+  def update
+    respond_to do |format|
+      if @todo.update(project_params)
+        format.html { redirect_to @todo, notice: 'Project was successfully updated.' }
+        format.json { render :show, status: :ok, location: @todo }
+      else
+        format.html { render :edit }
+        format.json { render json: @todo.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
 
   private
+  	def set_project
+      @todo = Todo.find(params[:id])
+    end
     def todo_params
       params.require(:todo).permit(:text, :isCompleted, :project_id)
     end
